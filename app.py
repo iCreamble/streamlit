46c0de6c4e9073914bdfb5e3c553290face82919
iCreamble <52177972+iCreamble@users.noreply.github.com>
2023-10-19 21:33:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
+from PIL import ImageOps
+from PIL import Image
 st.write('Lembre que eu amo muito você!')
+image = Image.open('IMG_3896.JPG')
+image = ImageOps.exif_transpose(image)
+st.image(image, 'nois')
